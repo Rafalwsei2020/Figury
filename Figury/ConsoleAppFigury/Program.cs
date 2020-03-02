2773df6f2c652d3ae5f3192e21cdf6ac0b44861a
Rafalwsei2020 <61200509+Rafalwsei2020@users.noreply.github.com>
2020-03-02 20:27:28 +0100

    wersja1 figury

---
+++ Figury/ConsoleAppFigury/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FiguryLibrary;
+
+namespace ConsoleAppFigury
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Trojkat t1; // delaracja zmiennej typu Trojkat 
+            t1 = new Trojkat(); // new operator tworzenia obiektu 
+
+    
+
+            Console.WriteLine(t1);
+            Console.WriteLine(t1.GetObwod());
+            Console.WriteLine(t1.Obwod);
+            Console.WriteLine($"pole = {t1.GetPole()}");
+            Console.WriteLine($"pole = {t1.Pole}");
+            t1.A = -1;
+            Console.WriteLine(t1);
+            Console.WriteLine(t1.Pole);
+            try
+            {
+                t1.A = -1;
+            }
+            catch(ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("nie wolno bokom przypisywać wartości ujemnych"); 
+            }
+
+            Console.WriteLine(t1);
+            Console.WriteLine(t1.Pole); 
+
+            Console.WriteLine("=========================");
+            var t2 = new Trojkat(-1, 2, 1);
+            Console.WriteLine(t2.A); 
+             
+             
+        }
+
+        
+
+
+       
+    }
+}
---
+++ Figury/FiguryLibrary/Trojkat.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace FiguryLibrary
+{
+    public class Trojkat
+    {
+        // definiujemy dane obiektu- pola obiektu (fields)
+     private  double a; // this.a
+        public double A
+        {
+            get { return a; }
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentOutOfRangeException("wartość A musi być dodatnia"); 
+                else
+                    a = value;
+            }
+        }
+
+     private double b;  //this.b
+        public double B
+        {
+            get => b; 
+            set
+
+            {
+                if (value <= 0)
+                    throw new ArgumentOutOfRangeException("wartość B musi być dodatnia");
+                else
+                    b = value;
+            }
+        }
+   private double c;  // thos.c   
+
+        public double C
+        {
+            get => c;
+            set
+
+            {
+                if (value <= 0)
+                    throw new ArgumentOutOfRangeException("wartość C musi być dodatnia");
+                else
+                    c = value;
+            }
+        }
+
+
+
+        // drugi krok opisanie procesu budowy obiektu (konstruktory)
+
+        public Trojkat() // konstruktor domyślny 
+        {
+            a = b = c = 1.0;
+        }
+
+        public Trojkat(double a, double b, double c)
+        {
+            if (a + b <= c || a + c <= a || b + c <= a)
+            {
+                throw new ArgumentException("niespełniony warunek trójkąta"); 
+
+            }
+            this.A = a;
+            this.B = b;
+            this.C = c;
+        }
+        public override string ToString()
+        {
+            return $"Trójkąt(a: {a}, b: {b}, c: {c})";
+        }
+
+        public double GetObwod()
+        {
+            return a + b + c;
+        }
+        
+
+        public double Obwod => a + b + c;
+
+        public double GetPole()
+        {
+            var p = 0.5 * GetObwod(); 
+            var s = Math.Sqrt(p * (p-a) *(p-b) * (p-c) );
+            return s; 
+        }
+
+        public double Pole
+        {
+            get
+            {
+                var p = 0.5 * Obwod;
+                var s = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+                return s; 
+            }
+        }
+        
+
+    }
+
+}
+
